908b2da08f447184d1e38af771a7e2221296a9d6
Johan <joppe_lorenz@hotmail.com>
2017-02-15 21:52:46 +0100

    new push

--- billigastpablocket/app/views.py
+++ billigastpablocket/app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import *
 from django.http import JsonResponse
+from datetime import datetime, timedelta
 
 def index(request):
 	
@@ -11,7 +12,29 @@ def index(request):
 def getSearch(request):
 	searchTerm = request.POST.get('search')
 	items = []
-	objects = Listing.objects.filter(name__contains=searchTerm)
+	last_month = datetime.today() - timedelta(days=30)
+	terms = searchTerm.split(" ")
+	if len(terms) == 1:
+		objects = Listing.objects.filter(name__icontains=searchTerm).filter(downloaded_at__gte=last_month)
+	elif len(terms) == 2:
+		objects = Listing.objects.filter(name__icontains=terms[0]).filter(name__icontains=terms[1]).filter(downloaded_at__gte=last_month)
+	elif len(terms) == 3:
+		objects = Listing.objects.filter(name__icontains=terms[0]).filter(name__icontains=terms[1]).filter(name__icontains=terms[2]).filter(downloaded_at__gte=last_month)
+	elif len(terms) == 4:
+		objects = Listing.objects.filter(name__icontains=terms[0]).filter(name__icontains=terms[1]).filter(name__icontains=terms[2]).filter(name__icontains=terms[3]).filter(downloaded_at__gte=last_month)
+	elif len(terms) == 5:
+		objects = Listing.objects.filter(name__icontains=terms[0]).filter(name__icontains=terms[1]).filter(name__icontains=terms[2]).filter(name__icontains=terms[3]).filter(name__icontains=terms[4]).filter(downloaded_at__gte=last_month)
+	elif len(terms) == 6:
+		objects = Listing.objects.filter(name__icontains=terms[0]).filter(name__icontains=terms[1]).filter(name__icontains=terms[2]).filter(name__icontains=terms[3]).filter(name__icontains=terms[4]).filter(name__icontains=terms[5]).filter(downloaded_at__gte=last_month)
+	elif len(terms) == 7:
+		objects = Listing.objects.filter(name__icontains=terms[0]).filter(name__icontains=terms[1]).filter(name__icontains=terms[2]).filter(name__icontains=terms[3]).filter(name__icontains=terms[4]).filter(name__icontains=terms[5]).filter(name__icontains=terms[6]).filter(downloaded_at__gte=last_month)
+	elif len(terms) == 8:
+		objects = Listing.objects.filter(name__icontains=terms[0]).filter(name__icontains=terms[1]).filter(name__icontains=terms[2]).filter(name__icontains=terms[3]).filter(name__icontains=terms[4]).filter(name__icontains=terms[5]).filter(name__icontains=terms[6]).filter(name__icontains=terms[7]).filter(downloaded_at__gte=last_month)
+	elif len(terms) == 9:
+		objects = Listing.objects.filter(name__icontains=terms[0]).filter(name__icontains=terms[1]).filter(name__icontains=terms[2]).filter(name__icontains=terms[3]).filter(name__icontains=terms[4]).filter(name__icontains=terms[5]).filter(name__icontains=terms[6]).filter(name__icontains=terms[7]).filter(name__icontains=terms[8]).filter(downloaded_at__gte=last_month)
+	else:
+		objects = Listing.objects.filter(name__icontains=searchTerm).filter(downloaded_at__gte=last_month)
+
 	if objects.count() == 0:
 		response = '{"errorResult" : "no-result"}'
 		bytes = JsonResponse(response, safe=False)
--- scrapers/scraper/spiders/blocket.py
+++ scrapers/scraper/spiders/blocket.py
@@ -17,39 +17,21 @@ class blocketSpider(scrapy.Spider):
         return [Request("https://www.blocket.se/hela_sverige/", callback=self.parse, meta={'index':1})]
         
     def parse(self, response):
-        items = response.xpath("//article[contains(@class, 'item_row')]//text()").extract();
-        if len(items) > 0:
-            x = 0
-            lst = []
-            lastLst = []
-            for i in items:
-                try:
-                    if x == 1 or x == 0:
-                        x += 1
-                        continue                       
+        items = response.xpath("//article[contains(@class, 'item_row')]")
+        for item in items:
+            try:
+                obj = Objekt()
+                obj['name'] = item.xpath(".//h1/a/text()").extract()[0].encode('utf-8')
+                obj['category'] = item.xpath(".//div[contains(@class, 'pull-left')]/a/text()").extract()[0].encode('utf-8')
+                obj['location'] = item.xpath(".//div[contains(@class, 'pull-left')]/text()").extract()[0].encode('utf-8')[3:]
+                obj['price'] = int(item.xpath(".//p[contains(@class, 'list_price')]/text()").extract()[0].encode('utf-8')[:-2].replace(" ", ""))
+                yield obj 
+            except:
+                pass
 
-                    if x == 7:
-                        x = 0
-                        item = Objekt()
-                        item['category'] = str(lst[0])
-                        item['location'] = str(lst[1][2:])
-                        item['name'] = str(lst[3])
-                        item['price'] = int(lst[4].split(':')[0].replace(" ",""))
-                        if lastLst != lst:
-                            yield item
-                            lst = []
-                            lastLst = lst
-                            continue
-                        else:
-                            continue
-                    
-                    if 1 < x <7:
-                        x += 1
-
-                    lst.append(i)
-                except:
-                    continue
-
+        try:
             request = scrapy.Request("https://www.blocket.se/hela_sverige/?ca=11&w=3&st=s&o=%s" % str(int(response.meta["index"])+1), callback=self.parse)
             request.meta['index'] = str(int(response.meta["index"]) + 1)
             yield request
+        except:
+            return
